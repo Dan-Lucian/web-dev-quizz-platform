2756aa85f9b5ab68bd8dab37b88ba9b65409f714
Dan-Lucian <danugaina1403@gmail.com>
2021-11-26 17:02:04 +0200

    feat: scroll to top on logo click

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,11 +1,25 @@
 import styled from 'styled-components';
 import LogoPath from '../img/Logo.png';
 
-const Logo = () => (
-  <StyledLogo width="50px" height="50px" src={LogoPath} alt="logo" />
-);
+const Logo = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <StyledLogo
+      onClick={scrollToTop}
+      width="50px"
+      height="50px"
+      src={LogoPath}
+      alt="logo"
+    />
+  );
+};
 
 const StyledLogo = styled.img`
+  cursor: pointer;
+
   @media (min-width: 1440px) {
     width: 60px;
     height: 60px;
