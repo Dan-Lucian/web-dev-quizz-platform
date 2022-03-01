84a6635e511a01d1593d9888086379dfe66ba2fa
Dan-Lucian <danugaina1403@gmail.com>
2022-03-01 21:16:52 +0200

    chore: setup for api testing
    * add cross-env library
    * change mongo uri based on NODE_ENV dev/prod/test
    * add supertest library

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,10 @@
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI;
 const SUBMIT_PASSWORD = process.env.SUBMIT_PASSWORD;
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 export { PORT, MONGODB_URI, SUBMIT_PASSWORD };
